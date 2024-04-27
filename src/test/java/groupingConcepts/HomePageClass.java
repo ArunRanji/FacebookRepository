8dbf4a61b8e840482dec777c17ac9a8f14ea6da1
selva Ranjitha <selvasutha31@gmail.com>
2024-04-27 09:30:56 +0530

    Home page class created

---
+++ src/test/java/groupingConcepts/HomePageClass.java
@@ -0,0 +1,10 @@
+package groupingConcepts;
+
+public class HomePageClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is home page class created by rajiv");
+	}
+
+}
